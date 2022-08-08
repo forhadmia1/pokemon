c49f80b4f7a7d96cd1ed3b0c04aba47dfb3c479e
forhadmia1 <forhad8613@gmail.com>
2022-08-09 02:20:12 +0600

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Pokemon</h2>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Header;
