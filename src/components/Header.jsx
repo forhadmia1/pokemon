8c62194e51de583ad06a1685289173d16cb89e8d
forhadmia1 <forhad8613@gmail.com>
2022-08-09 02:56:21 +0600

    header create

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Pokemon</h2>
-            <div>
-                
+        <div className='flex justify-between items-center py-4 bg-sky-400 px-24'>
+            <h2 className='text-3xl font-bold text-white'>Pokemon</h2>
+            <div class="xl:w-96">
+                <div className='flex'>
+                    <input type="search" class=" border-sky-600 px-1 py-2 focus:outline-none rounded-lg border-2 w-80"/>
+                <button class="bg-sky-600 px-4 py-1 ml-[-6px] rounded-r-lg hover:bg-sky-700">Search</button>
+                </div>
             </div>
         </div>
     );
